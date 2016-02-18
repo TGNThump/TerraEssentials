896d6fabdddd54d491d9f7e823afae4f18b6e26b
Ben Pilgrim <ben@pilgrim.me.uk>
2016-02-18 14:23:57 +0000

    Split /TerraEssentials from /TerraCore.
    Switch to Sponge Plugin archetype.

---
+++ src/test/java/uk/co/terragaming/MyTest.java
@@ -0,0 +1,12 @@
+package uk.co.terragaming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyTest {
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+}
